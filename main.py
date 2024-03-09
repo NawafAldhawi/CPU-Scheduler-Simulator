84736f509a855bc667f6ffd8a8da19f4bdda631b
NawafAldhawi <162730046+NawafAldhawi@users.noreply.github.com>
2024-03-09 10:58:53 +0300

    Update main.py
    -Changed the poor time implementation
    -Incorporated arrival time as part of the process picking in FCFS
    -Added Priority Queue
    -Now the program calculates Average turnaround and waiting time
    -Minor function naming changes 
    -Minor visual changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
 class Process:
     def __init__(self,pid,burst_time,arrival_time,prio):
         self.pid = pid
-        self.burst_time = burst_time/1000
+        self.burst_time = burst_time
         self.arrival_time = arrival_time
         self.prio = prio
 
@@ -14,39 +13,53 @@ class Scheduler:
         self.process_queue.extend(processes)
 
     def handle_processes(self):
-        start_time = time.time()
-        sum_arrival_time = 0
+
         sum_burst_time = 0
+        sum_waiting_time = 0
         processes = []
+        total_turnaround_time = 0
+
         while self.process_queue:
+
             current_process = self.process_pick()
 
-            current_process.arrival_time = round(time.time() - start_time,4)
-            sum_arrival_time += current_process.arrival_time
+            waiting_time = sum_burst_time
             sum_burst_time += current_process.burst_time
-            spaces = round(current_process.burst_time * 1000 // 2)
-            time.sleep(current_process.burst_time)
+
+            turnaround_time = sum_burst_time
+            total_turnaround_time += turnaround_time
+
+            sum_waiting_time += waiting_time
+
+
+            spaces = round(current_process.burst_time // 2)
             print('-{', ' ' * spaces, current_process.pid, ' ' * spaces, '}-', end='')
-            processes.append(current_process)
-        print(f'\nAverage Burst Time: {round(sum_burst_time * 1000 / len(processes),4)}')
-        print(f'Average Arrival Time: {round(sum_arrival_time * 1000 / len(processes),4)}')
+            processes.append([current_process,waiting_time])
+
+        print(f'\n\nAverage Turnaround Time: {round((total_turnaround_time/len(processes)),3)}')
+        print(f'Average Waiting Time: {round((sum_waiting_time/len(processes)),3)}')
+
 
 class FCFS(Scheduler):
 
-    def FCFS_handle_queue(self):
+    def handle_queue(self):
         #clock start
-        print('FCFS Queue Scheduler:')
+        print('\nFCFS Queue Scheduler:\n')
         self.handle_processes()
 
     def process_pick(self):
-        return self.process_queue.pop(0)
-
+        picked_process = self.process_queue[0]
+        for process in self.process_queue:
+            if process.arrival_time < picked_process.arrival_time:
+                picked_process = process
+        self.process_queue.remove(picked_process)
+        return picked_process
 
 
 class SJF(Scheduler):
 
-    def SJF_handle_queue(self):
-        print('SFJ Queue Scheduler:')
+    def handle_queue(self):
+        print('\nSJF Queue Scheduler:\n')
         self.handle_processes()
 
 
@@ -58,21 +71,35 @@ class SJF(Scheduler):
         self.process_queue.remove(picked_process)
         return picked_process
 
+
+class PriorityQueue(Scheduler):
+
+
+    def handle_queue(self):
+        print('\nPriority Queue Scheduler:\n')
+        self.handle_processes()
+
+
+    def process_pick(self):
+        picked_process = self.process_queue[0]
+        for process in self.process_queue:
+            if process.prio < picked_process.prio:
+                picked_process = process
+        self.process_queue.remove(picked_process)
+        return picked_process
+
 class RR(Scheduler):
 
     def RR_handle_queue(self):
         pass
 
-class PriorityQueue(Scheduler):
-
-    def PriorityQueue_handle_queue(self):
-        pass
 
 
 sjf = SJF()
-p1 = Process(1,5,0,0)
-p2 = Process(2,2,0,0)
-p3 = Process(3,3,0,0)
-lst = [p1,p2,p3]
+p1 = Process(1,1,1,1)
+p2 = Process(2,2,2,4)
+p3 = Process(3,3,3,2)
+p4 = Process(4,12,4,5)
+lst = [p1,p2,p3,p4]
 sjf.load_processes(lst)
-sjf.SJF_handle_queue()
+sjf.handle_queue()
