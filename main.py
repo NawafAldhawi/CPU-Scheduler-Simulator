f84d1016d87e526b61000ac4b2984ce005ddc8cc
NawafAldhawi <162730046+NawafAldhawi@users.noreply.github.com>
2024-03-15 17:35:22 +0300

    Update main.py
    Added a clock for more accurate calculation.
    i didnt notice that there was many errors regarding turnaround and waiting time caculations. now i think its all fixed
    
    still havent implemented a clock for the RR.
    
    also started adding/added comments

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 class Process:
-    def __init__(self,pid,burst_time,arrival_time,prio):
+
+    def __init__(self,pid,burst_time,arrival_time,prio,quanta=3):
         self.pid = pid
         self.burst_time = burst_time
         self.arrival_time = arrival_time
         self.prio = prio
+        self.quanta = quanta
 
 class Scheduler:
+
     def __init__(self):
         self.process_queue= []
 
@@ -14,32 +17,42 @@ class Scheduler:
 
     def handle_processes(self):
 
-        sum_burst_time = 0
-        processes = []
+        clock = 0                   #the time the process enters the cpu
+        sum_burst_time = 0          #used mainly to calculate the clock
+        processes = []               #no signifcant uses except keep track of number of processes
         total_turnaround_time = 0
         total_waiting_time = 0
 
         while self.process_queue:
 
+
             current_process = self.process_pick()
 
-            waiting_time = sum_burst_time - current_process.arrival_time
+
+            clock = sum_burst_time + 1      #Entry time of a process
+
+            if sum_burst_time < current_process.arrival_time:
+                clock = current_process.arrival_time
+
+            #nonpremitve wait time of each process
+            waiting_time = clock - current_process.arrival_time
+            if waiting_time <0:
+                waiting_time = 0
             total_waiting_time += waiting_time
+
+
             sum_burst_time += current_process.burst_time
 
-            turnaround_time = sum_burst_time - current_process.arrival_time
+
+            turnaround_time = abs(clock +current_process.burst_time - current_process.arrival_time)
             total_turnaround_time += turnaround_time
 
 
-
+            #visualisation of output
             spaces = round(current_process.burst_time // 2)
             print('-{', ' ' * spaces, current_process.pid, ' ' * spaces, '}-', end='')
             processes.append(current_process)
 
-
-        if isinstance(self,SJF):
-            total_waiting_time += 1
-            total_turnaround_time += 1
 
         print(f'\n\nAverage Turnaround Time: {round((total_turnaround_time/len(processes)),4)}')
         print(f'Average Waiting Time: {round((total_waiting_time/len(processes)),4)}')
@@ -48,11 +61,11 @@ class Scheduler:
 class FCFS(Scheduler):
 
     def handle_queue(self):
-        #clock start
         print('\nFCFS Queue Scheduler:\n')
         self.handle_processes()
 
     def process_pick(self):
+        #pick process with least arrival time then remove it from the queue
         picked_process = self.process_queue[0]
         for process in self.process_queue:
             if process.arrival_time < picked_process.arrival_time:
@@ -69,6 +82,7 @@ class SJF(Scheduler):
 
 
     def process_pick(self):
+        #pick process with least burst time then remove it from the queue
         picked_process = self.process_queue[0]
         for process in self.process_queue:
             if process.burst_time < picked_process.burst_time:
@@ -86,6 +100,7 @@ class PriorityQueue(Scheduler):
 
 
     def process_pick(self):
+        # pick process with highest priority (lowest prio number) then remove it from the queue
         picked_process = self.process_queue[0]
         for process in self.process_queue:
             if process.prio < picked_process.prio:
@@ -99,7 +114,7 @@ class RR(Scheduler):
         print('\nRR Queue Scheduler:\n')
         self.RR_handle_processes()
 
-    def RR_handle_processes(self):
+    def RR_handle_processes(self): #special handeling for RR
 
         waiting_time = 0
         sum_burst_time = 0
@@ -111,28 +126,33 @@ class RR(Scheduler):
 
             current_process = self.process_pick()
 
+
             if current_process.burst_time < current_process.quanta:
+                #when process burst time is less than the quanta then
+                #the time added to burst time is just the remaining burst
                 sum_burst_time += current_process.burst_time
             else:
+                #otherwise it is the full quanta
                 sum_burst_time += current_process.quanta
 
 
             current_process.burst_time -= current_process.quanta
 
-            if current_process.burst_time <= 0:
-
-                current_process.quanta = current_process.burst_time + current_process.quanta
+            if current_process.burst_time <= 0: #means the process is fully executed
 
                 turn_around_time = sum_burst_time - current_process.arrival_time
                 total_turnaround_time += turn_around_time
                 self.process_queue.remove(current_process)
-                processes.append(current_process)
+
+                processes.append(current_process) #just a tracker list (used for len() )
+
 
             else:
+                #otherwise process is not finished send it back to the queue
                 self.process_queue.remove(current_process)
                 self.process_queue.append(current_process)
 
-
+            #after all processes are done, the total waiting time = total turnaround time + total burst time
             total_waiting_time = total_turnaround_time - sum_burst_time
             spaces = round(current_process.quanta // 2)
             print('-{', ' ' * spaces, current_process.pid, ' ' * spaces, '}-', end='')
@@ -142,17 +162,15 @@ class RR(Scheduler):
 
     def process_pick(self):
         picked_process = self.process_queue[0]
-        for process in self.process_queue:
-            process.quanta = 3
         return picked_process
 
 
 
-sjf = SJF()
-p1 = Process(1,3,0,1)
-p2 = Process(2,5,2,4)
-p3 = Process(3,2,3,2)
-p4 = Process(4,19,4,5)
+sjf = FCFS()
+p1 = Process(1,1,1,1)
+p2 = Process(2,2,2,2)
+p3 = Process(3,3,3,3)
+p4 = Process(4,4,30,4)
 lst = [p1,p2,p3,p4]
 sjf.load_processes(lst)
 sjf.handle_queue()
